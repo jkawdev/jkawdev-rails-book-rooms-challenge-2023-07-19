5381fa479edcb382469df864aa04ba216f9129e2
VincentBeaumont <beaumontvincent@yahoo.fr>
2023-07-19 15:59:57 +0200

    creation_seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  validates :starts_at, :ends_at
+  validates :starts_at, :ends_at, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Hotel.delete_all
+Room.delete_all
+Booking.delete_all
+
+Hotel.create!(name: "Formule 1", address: "Meudon")
+Hotel.create!(name: "La Dernière Chance", address: "Eu")
+Hotel.create!(name: "Au Lion d'or", address: "Saint Pourçain sur Sioule")
+
+
+60.times do
+  price = rand(10..35)
+  price_division = price % 5
+  if price_division <= 2
+    price_per_night = price - price_division
+  else
+    price_per_night = price + price_division
+  end
+
+  room = {
+    hotel_id: rand(1..3),
+    price_per_night: price_per_night,
+    capacity: rand(1..4)
+  }
+  Room.create!(room)
+end
