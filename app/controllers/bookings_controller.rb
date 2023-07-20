715323b02c490cd1ed7895dc28801852c75ce609
VincentBeaumont <beaumontvincent@yahoo.fr>
2023-07-20 17:50:13 +0200

    button_show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,6 +7,8 @@ class BookingsController < ApplicationController
     @room = Room.find(params[:room_id])
     @booking = Booking.new(booking_params)
     @booking.room = @room
+    puts "starts_at: #{booking_params[:starts_at]}"
+    puts "ends_at: #{booking_params[:ends_at]}"
     if @booking.save
       redirect_to room_path(@room), notice: "Bravo, réservation réussie"
     else
@@ -17,6 +19,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:starts_at, :ends_at)
+    params.require(:booking).permit(:starts_at, :ends_at, :room_id)
   end
 end
